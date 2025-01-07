eadf0ca6621c6fe201d5f2e4f21a0e4df6059827
morgana-x <89588301+morgana-x@users.noreply.github.com>
2025-01-07 20:44:56 +1100

    Rename CFSI_ExtractPack

---
+++ ZE_CFSI_Extract/Program.cs
@@ -0,0 +1,25 @@
+﻿using ZE_CFSI_Lib;
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath = "";
+        if (args.Length ==0)
+        {
+            Console.WriteLine("Drag and drop file to extract!");
+            filePath = Console.ReadLine().Replace("\"", "");
+        }
+        else
+            filePath = args[0];
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"File {filePath} doesn't exist!");
+            return;
+        }
+
+        Console.WriteLine("Extracting...");
+        new CFSI_Lib(filePath).ExtractAll(filePath + "_extracted");
+        Console.WriteLine("Extracted!");
+    }
+}
