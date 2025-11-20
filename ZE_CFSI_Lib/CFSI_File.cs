338976ff18f3cad940796760b573b9475f5c0b18
Neptune <595554963@qq.com>
2025-11-20 11:29:37 +0800

    Update CFSI_File.cs

--- ZE_CFSI_Lib/CFSI_File.cs
+++ ZE_CFSI_Lib/CFSI_File.cs
@@ -1,12 +1,35 @@
-﻿namespace ZE_CFSI_Lib
+using System.Text.Json.Serialization;
+
+namespace ZE_CFSI_Lib
 {
     public class CFSI_File
     {
-        public string Path { get; set; } // Full path (Within archive)
-        public string Name { get; set; } // File name and extension
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("size")]
         public uint Size { get; set; }
-        public uint Offset { get; set; } // Offset relative to Data section
+
+        [JsonPropertyName("offset")]
+        public uint Offset { get; set; }
+
+        [JsonPropertyName("compressed")]
         public bool Compressed { get; set; } = false;
+
+        [JsonPropertyName("originalHeaderBytes")]
+        public byte[]? OriginalHeaderBytes { get; set; }
+
+        [JsonPropertyName("originalHeaderPosition")]
+        public long OriginalHeaderPosition { get; set; }
+
+        [JsonPropertyName("originalOffsetValue")]
+        public uint OriginalOffsetValue { get; set; }
+
+        [JsonPropertyName("originalSizeValue")]
+        public uint OriginalSizeValue { get; set; }
 
         public CFSI_File(string name, string path, uint offset, uint size)
         {
